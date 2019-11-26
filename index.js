c87397306681986d098865597c3ff846b7fcb78f
besteman <ellioy37@gmail.com>
2019-11-25 19:13:28 -0500

    Search for one actor done

--- index.js
+++ index.js
@@ -2,16 +2,44 @@ import request from 'request';
 
 require('dotenv').config();
 
-const url = `https://api.themoviedb.org/3/search/person?include_adult=false&page=1&query=nathan%20fillion&language=en-US&api_key=${process.env.movieDatabaseApiKey}`;
+const personSearchUrl = `https://api.themoviedb.org/3/search/person?include_adult=false&page=1&query=nathan%20fillion&language=en-US&api_key=${process.env.movieDatabaseApiKey}`;
 
-const options = {
-  method: 'GET',
-  url,
+const getMoviesByActor = (actorId) => {
+  const movieCreditsUrl = `https://api.themoviedb.org/3/person/${actorId}/movie_credits?api_key=${process.env.movieDatabaseApiKey}&language=en-US`;
+
+  const options = {
+    method: 'GET',
+    url: movieCreditsUrl,
+  };
+
+  request(options, (err, response, body) => {
+    if (err) {
+      throw err;
+    }
+
+    const temp = JSON.parse(body);
+
+    console.log(temp);
+  });
 };
 
-request(options, (err, response, body) => {
-  if (err) {
-    throw err;
-  }
-  console.log(body);
-});
+
+const getActorId = () => {
+  const options = {
+    method: 'GET',
+    url: personSearchUrl,
+  };
+  request(options, (err, response, body) => {
+    if (err) {
+      throw err;
+    }
+
+    const temp = JSON.parse(body);
+
+    console.log(temp.results[0].id);
+
+    getMoviesByActor(temp.results[0].id);
+  });
+};
+
+getActorId();
